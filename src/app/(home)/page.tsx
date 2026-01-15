6e7a94567162803ae83544db6b3bc67477054a29
5xdf <nbhogwarts@outlook.com>
2026-01-15 13:01:03 -0500

    Update welcome message on HomePage for clarity

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -5,11 +5,11 @@ export default function HomePage() {
     <div className="flex flex-col justify-center text-center flex-1">
       <h1 className="text-2xl font-bold mb-4">Hello World</h1>
       <p>
-        You can open{' '}
+        Welcome to the Hexlane Docs! You can go to {' '}
         <Link href="/docs" className="font-medium underline">
           /docs
         </Link>{' '}
-        and see the documentation.
+        to see the documentation.
       </p>
     </div>
   );
